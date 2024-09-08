59dc5b3a411c9ab4b958a9e5c3b0f7f9053e5dc5
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2024-09-08 12:47:39 -0700

    Whoop whoop, you can win

--- TicTacToeEngine.py
+++ TicTacToeEngine.py
@@ -2,15 +2,17 @@ class TicTacToeEngine:
     def __init__(Self, Size) -> None:
         Self.Size = Size
         Self.Build_Board()
-        Self.Winner, Self.Loser = None, None
+        Self.Winner = None
         Self.PlayerOne = "X"
         Self.PlayerTwo = "O"
         Self.CurrentPlayer = Self.PlayerOne
         Self.CurrentMove = None
+        Self.WinningCases = {"X" * Self.Size:"X",
+                             "O" * Self.Size:"O",}
         
-        while [Self.Winner, Self.Loser] == [None, None]:
-            Self.User_Turn()
+        while Self.Winner == None:
             Self.Render_Board()
+            Self.User_Turn()
             if Self.CurrentPlayer == Self.PlayerOne:
                 Self.CurrentPlayer = Self.PlayerTwo
             else:
@@ -26,21 +28,74 @@ class TicTacToeEngine:
             Self.Board.update({Y:Self.Row})
 
 
-    def User_Turn(Self, OutOfBounds=False, SpotTaken=False) -> None:
+    def Check_If_Winning_Row(Self) -> bool:
+        Row = Self.Board[Self.CurrentMove[0]]
+        MatchCase ="".join([Character for Character in Row.values()])
+        if MatchCase in Self.WinningCases.keys():
+            Self.Winner = Self.WinningCases[MatchCase]
+            return True
+        return False
+
+
+    def Check_If_Winning_Column(Self) -> bool:
+        MatchCase = "".join([list(Self.Board[Row].values())[Self.CurrentMove[1]] for Row in Self.Board])
+        if MatchCase in Self.WinningCases.keys():
+            Self.Winner = Self.WinningCases[MatchCase]
+            return True
+        return False
+
+
+    def Check_If_Winning_Diagonals(Self) -> bool:
+        MatchCaseLeft = "".join([list(Self.Board[Row].values())[Index] for Index, Row in enumerate(Self.Board)])
+        MatchCaseRight = "".join([list(Self.Board[Row].values())[(Self.Size - 1) - Index] for Index, Row in enumerate(Self.Board)])
+        if MatchCaseLeft in Self.WinningCases.keys():
+            Self.Winner = Self.WinningCases[MatchCaseLeft]
+            return True
+        if MatchCaseRight in Self.WinningCases.keys():
+            Self.Winner = Self.WinningCases[MatchCaseRight]
+            return True
+        return False
+
+
+    def Check_If_Winning_Board(Self) -> bool:
+        if Self.Check_If_Winning_Row():
+            return True
+        if Self.Check_If_Winning_Column():
+            return True
+        if Self.Check_If_Winning_Diagonals():
+            return True
+        return False
+
+
+    def User_Turn(Self, OutOfBounds=False, SpotTaken=False, InvalidInput=False) -> None:
         if OutOfBounds == True:
             print("You're previous move was not within the bounds of the board.\n")
+            Self.Render_Board()
         if SpotTaken == True:
             print("That position is taken already.")
-        UserMove = input(f"Alright {Self.CurrentPlayer}'s, what is your move? (Formatting: RowNumber, ColumnNumber)\n")
+            Self.Render_Board()
+        if InvalidInput == True:
+            print("Invalid input. Formatting: RowNumber, ColumnNumber. Example: 2,3")
+            Self.Render_Board()
+        UserMove = input(f"Alright {Self.CurrentPlayer}'s, what is your move?\n")
         MovePosition = UserMove.split(",")
+        if len(MovePosition) != 2 or MovePosition[0].isdigit() == False or MovePosition[1].isdigit() == False:
+            Self.User_Turn(InvalidInput=True)
+            return
         MoveY, MoveX = int(MovePosition[0])-1, int(MovePosition[1])-1
         Self.CurrentMove = [MoveY, MoveX]
         if MoveY > Self.Size-1 or MoveY < 0 or MoveX > Self.Size-1 or MoveX < 0:
             Self.User_Turn(WarnOutOfBoundsing=True)
+            return
         elif Self.Board[MoveY][MoveX] in ["X", "O"]:
             Self.User_Turn(SpotTaken=True)
+            return
         else:
             Self.Board[MoveY][MoveX] = Self.CurrentPlayer
+        
+        if Self.Check_If_Winning_Board() == True:
+            print(f"{Self.Winner} has won!")
+            Self.Render_Board()
 
 
     def Render_Board(Self) -> None:
--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
 if __name__ == "__main__":
+    from sys import argv as Arguments
     from TicTacToeEngine import TicTacToeEngine
-    Engine = TicTacToeEngine(3)
+    ArgumentsCount = len(Arguments)
+    if ArgumentsCount == 2 and Arguments[1].isdigit() == True:
+        Engine = TicTacToeEngine(int(Arguments[1]))
+    else:
+        Engine = TicTacToeEngine(3)
